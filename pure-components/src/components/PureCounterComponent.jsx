5f4757d61c5e477a3d7d0b7202e5feb476264176
unknown <vaishnavi.salunkhe@kalvium.community>
2024-01-31 18:12:45 +0530

    Made changes

--- pure-components/src/components/PureCounterComponent.jsx
+++ pure-components/src/components/PureCounterComponent.jsx
@@ -1,31 +1,35 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from 'react'
 
 export default class PureCounterComponent extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      toggle: false,
-    };
-  }
 
-  
-  render() {
-    console.log("This is pure component.")
-      const { count, toggle } = this.state;
-      
-      let handleClick = () => {
-        if (toggle) {
-          this.setState({ count:count + 1 });
+    constructor(){
+        super()
+        this.state = {
+            toggle : false,
+            count : 0
         }
-      };
-    return (
-      <div>
-        <h2>Pure Component</h2>
-        <h3>{count}</h3>
-        <button onClick={() => this.setState({ toggle: !toggle })}>setToggle</button>
-        <button onClick={handleClick}>Counter</button>
-      </div>
-    );
-  }
+    }
+
+    render() {
+        console.log("This is Pure Component")
+        const {count , toggle} = this.state
+
+        let handleClick = () =>{
+            if (toggle){
+                this.setState({
+                    count : count + 1
+                })
+            }
+        }
+
+        return (
+            <div>
+                <h2>Pure Component</h2>
+                <h3>{count}</h3>
+
+                <button onClick={()=>{this.setState({toggle : !toggle})}}>Set Toggle</button>
+                <button onClick={handleClick}>Counter</button>
+            </div>
+        )
+    }
 }
--- pure-components/src/components/SimpleCounterComponent.jsx
+++ pure-components/src/components/SimpleCounterComponent.jsx
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class SimpleCounterComponent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      toggle: false,
-    };
-  }
 
-  
-  render() {
-    console.log("This is simple component.")
-
-      const { count, toggle } = this.state;
-      
-      let handleClick = () => {
-        if (toggle) {
-          this.setState({ count:count + 1 });
+    constructor(){
+        super()
+        this.state = {
+            toggle : false,
+            count : 0
         }
-      };
-    return (
-      <div>
-        <h2>Simple Component</h2>
-        <h3>{count}</h3>
-        <button onClick={() => this.setState({ toggle: !toggle })}>setToggle</button>
-        <button onClick={handleClick}>Counter</button>
-      </div>
-    );
-  }
+    }
+
+    render() {
+        console.log("This is a Simple Component")
+        const {count , toggle} = this.state
+
+        let handleClick = () =>{
+            if (toggle){
+                this.setState({
+                    count : count + 1
+                })
+            }
+        }
+
+        return (
+            <div>
+                <h2>Simple Component</h2>
+                <h3>{count}</h3>
+
+                <button onClick={()=>{this.setState({toggle : !toggle})}}>Set Toggle</button>
+                <button onClick={handleClick}>Counter</button>
+            </div>
+        )
+    }
 }
